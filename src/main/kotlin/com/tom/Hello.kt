f8cbd0e8ca30665e419273fb4d78d01e9f1878c4
guang <fju505170491@outlook.com>
2019-05-31 13:40:18 +0800

    test

---
+++ src/main/kotlin/com/tom/Hello.kt
@@ -0,0 +1,14 @@
+package com.tom
+
+fun main(args: Array<String>) {
+//    println("Hello Kotlin")
+    val h = Human()
+    h.hello()
+
+}
+
+class Human{
+    fun hello(){
+        println("Hello Kotlin")
+    }
+}
